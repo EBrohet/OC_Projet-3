9a690387ff74e10eebd1234bfd42c453345ea2d0
Emilie Brohet <emilie.brohet@gmail.com>
2024-02-25 15:34:56 +0100

    corrections et intégration de la modale

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,32 +1,27 @@
-function connexion() {
-    const connexion = document.querySelector(".btn-envoyer");
-    connexion.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const identifiantsConnexion = {
-            email:document.querySelector("#email").value,
-            password: document.querySelector("#password").value
-        };
+const form = document.querySelector("#form");
+form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const identifiantsConnexion = {
+        email:document.querySelector("#email").value,
+        password: document.querySelector("#password").value
+    };   
+    const res = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(identifiantsConnexion)
     })
-    async function reponse () {
-        const res = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(identifiantsConnexion)
+    if (res.ok === true) {
+        return res.json()
+        .then(data => {
+            localStorage.setItem("token", data.token);
+            window.location.href = "index.html";
         })
-        if (res.ok === true) {
-            return res.json()
-            .then(data => {
-                const token = data.token;
-                localStorage.setItem("token", token);
-                window.location.href = "index.html";
-            })
-        } else {
-            throw new Error ("Erreur dans l'identifiant ou le mot de passe");
-        }
+    } else {
+        const button = document.querySelector(".btn-envoyer");
+        const erreur = document.createElement("p");
+        erreur.setAttribute ("style", "text-align: center; margin-bottom: 15px; text-decoration: none;")
+        erreur.innerText = "Erreur dans l'identifiant ou le mot de passe";
+        form.insertBefore(erreur, button);
+        console.log("Erreur dans l'identifiant ou le mot de passe");
     }
-}
-
-
-// const token = localStorage.getItem('token');
-// console.log(token);
-
+})
--- FrontEnd/JS/works.js
+++ FrontEnd/JS/works.js
@@ -77,17 +77,16 @@ function admin() {
     boutonModifier.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>modifier';
     const logout = document.querySelector(".logout");
     logout.innerHTML = "logout";
+
+    logout.addEventListener("click", function() {
+        localStorage.removeItem("token");
+        window.location.href = "index.html";
+    })
 };
 
-// function seConnecter() {
-//     const token = localStorage.getItem('token');
-//     console.log(token);
-//     if(token) {
-//         admin();
-//     };
-// };
-
-// admin();
+if("token" in localStorage) {
+    admin();
+};
 
 
 
@@ -95,13 +94,27 @@ function admin() {
 
 const open = document.querySelector(".btn-modifier");
 const dialog = document.querySelector("dialog");
-const close = document.querySelector(".fermer");
+const close = document.querySelector(".btn-fermer");
 const modale = document.querySelector(".modale");
+const modale2 = document.querySelector(".modale2");
 
 modale.innerHTML = `<h3>Galerie photo</h3>
-                    <i class="fa-solid fa-trash-can fa-border"></i>
+                    <div class="mini-photos">
+                        
+                    </div>
                     <hr>
                     <button class="ajout-photo">Ajouter une photo</button>`;
+
+modale2.innerHTML = `<p class="btn-fleche"><i class="fa-solid fa-arrow-left"></i></p>
+                     <h3>Ajout photo</h3>
+                     <div class="photo">
+                        <i class="fa-regular fa-image fa-4x"></i>
+                        <button>+ Ajouter photo</button>
+                        <p>jpg, png : 4mo max</p>
+                     </div>
+                     <form></form>
+                     <hr>
+                     <button class="ajout-photo">valider</button>`;
 
 open.addEventListener("click", () => {
     dialog.showModal();
@@ -113,5 +126,30 @@ close.addEventListener("click", () => {
 
 const ajout = document.querySelector(".ajout-photo");
 ajout.addEventListener("click", function() {
-    modale.innerHTML = ``;
+    modale.setAttribute("style", "display: none;")
+    modale2.setAttribute("style", "display: block;")
 });
+
+const retour = document.querySelector(".btn-fleche");
+retour.addEventListener("click", function () {
+    modale2.setAttribute("style", "display: none;")
+    modale.setAttribute("style", "display: block;");       
+});
+
+
+for (let i=0; i < works.length; i++) {
+    const imageElement = document.createElement("img");
+    imageElement.src = works[i].imageUrl;
+    const figure = document.createElement("figure");
+
+    const miniatures = document.querySelector(".mini-photos");
+    miniatures.appendChild(figure);
+    figure.appendChild(imageElement);
+
+    const poubelle = document.createElement("div");
+    poubelle.classList.add("btn-effacer");
+    const icone = document.createElement("i");
+    icone.classList.add("fa-solid", "fa-trash-can", "fa-border");
+    figure.appendChild(poubelle);
+    poubelle.appendChild(icone);
+}
